6050929bac1d6f8f0882c365a6c657657249db75
vsinvi23 <39766053+vsinvi23@users.noreply.github.com>
2026-09-19 14:23:58 +0530

    chore(release): build and sync production SPA assets into go-cms dist

--- gg-cms/backend/go-cms/dist/assets/useDomains-BSRzxcjc.js
+++
@@ -1,1 +0,0 @@
-import{n as e,o as s}from"./index-B9R3FP-i.js";const a={async getAll(){return(await e.get("/domains")).data.data||[]}},o=()=>s({queryKey:["domains"],queryFn:a.getAll,staleTime:5*6e4});export{o as u};
